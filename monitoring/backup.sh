d1428afd65288f0066fcf8d16d8f408b4c026674
Ayra Stelmaszewski <68444023+AyraStelmaszewski@users.noreply.github.com>
2024-04-12 18:45:04 +0200

    Update backup.sh

--- monitoring/backup.sh
+++ monitoring/backup.sh
@@ -3,9 +3,6 @@
 # Set the directory where the CSV file will be saved
 output_dir="/home/ayra/monitoring"
 
-# Create the output directory if it doesn't exist
-mkdir -p "$output_dir"
-
 # Generate the filename with the current date
 filename="weeklybackup$(date +"%Y%m%d-%H%M").csv"
 
